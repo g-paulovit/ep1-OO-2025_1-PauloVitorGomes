98d72a9645d20d18c763dcf25694f7f661e52573
Paulo Vitor Gomes <bpaulovitorgomes@gmail.com>
2025-05-19 13:08:08 -0300

    Classes

---
+++ untitled/src/Aluno.java
@@ -0,0 +1,8 @@
+public class Aluno {
+    private String nome;
+    private String curso;
+    private String email;
+    private String senha;
+
+
+}
---
+++ untitled/src/Professor.java
@@ -0,0 +1,7 @@
+public class Professor {
+    private String nome;
+    private String curso;
+    private String email;
+    private String ;
+
+}
